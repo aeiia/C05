5f7f8f662e5ef8167c2cbb4b2c4f886a5ca211cb
tiida <takehi.iida@gmail.com>
2025-11-28 00:59:48 +0900

    ex00 ft_iterative_factorial

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: tiida <tiida@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 00:18:24 by tiida             #+#    #+#             */
-/*   Updated: 2025/11/28 00:50:38 by tiida            ###   ########.fr       */
+/*   Updated: 2025/11/28 00:59:03 by tiida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@ int	ft_iterative_factorial(int nb)
 	int	result;
 
 	result = 1;
-	while (nb != 1)
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	while (nb > 1)
 	{
 		result = result * nb;
 		nb--;
